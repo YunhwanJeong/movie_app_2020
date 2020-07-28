75e55cf41ba215a69f9821371756c12e4e8224c0
aiden-jung <jimmy53120488@gmail.com>
2020-07-28 16:11:14 +0900

    Updates

---
+++ precache-manifest.d1e69e43916b9510d9db723ca778080b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490a364ac1150788b60d074fd10e4ea2",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "50e072386cba2412ddaf",
+    "url": "/movie_app_2020/static/js/2.569c14e9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/movie_app_2020/static/js/2.569c14e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03a4449daed51449d19",
+    "url": "/movie_app_2020/static/js/main.95bb4914.chunk.js"
+  },
+  {
+    "revision": "5b9de689d76c950937df",
+    "url": "/movie_app_2020/static/js/runtime-main.db2dcdc9.js"
+  }
+]);
